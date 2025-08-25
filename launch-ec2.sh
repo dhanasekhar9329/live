b7c7cafba29b87239383964c5e3c8c03d025b96e
dhanasekhar9329 <145316942+dhanasekhar9329@users.noreply.github.com>
2025-08-25 15:44:45 +0530

    Update launch-ec2.sh

--- launch-ec2.sh
+++ launch-ec2.sh
@@ -5,13 +5,13 @@ set -x
 
 # Set AWS region and security group/key pair
 REGION="us-east-1"
-SECURITY_GROUP="sg-074b393c038227f9e"  # Replace with your actual security group ID
-KEY_PAIR="demo"  # Just the name of the key pair, not the .pem file
+SECURITY_GROUP="sg-04f749d01bddcd0d5"  # Replace with your actual security group ID
+KEY_PAIR="key1"  # Just the name of the key pair, not the .pem file
 INSTANCE_TYPE="t2.micro"  # Example instance type, adjust as needed
 
  # Set AWS credentials (Make sure to store them securely)
-export AWS_ACCESS_KEY_ID="AKIAVRUVVR5ISFUIGMOR"
-export AWS_SECRET_ACCESS_KEY="aOjQ1+ckLtXHtJnJ36ZodT6uM+akTsr7SKSYBxtu"
+export AWS_ACCESS_KEY_ID="AKIA5IUM3ZHEYH5O6E75"
+export AWS_SECRET_ACCESS_KEY="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"
 export AWS_DEFAULT_REGION="$REGION"
 
 # Generate a random name for the instance
@@ -19,7 +19,7 @@ INSTANCE_NAME="Instance-$(date +%s)-$RANDOM"
 
 # Launch EC2 instance with random name and other parameters
 INSTANCE_ID=$(aws ec2 run-instances \
-  --image-id ami-01816d07b1128cd2d \
+  --image-id ami-00ca32bbc84273381 \
   --count 1 \
   --instance-type $INSTANCE_TYPE \
   --key-name $KEY_PAIR \
